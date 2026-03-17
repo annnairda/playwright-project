107999bf111a3572013f032fc6061ea2bcc9fe4d
adrmen_acxiom <adrianna.mendalka@acxiom.com>
2026-03-17 11:23:07 +0100

    add tags and addtional tests to signin.spec.js

--- tests/signin.spec.js
+++ tests/signin.spec.js
@@ -1,16 +1,13 @@
 // @ts-check
 import { test, expect } from '@playwright/test';
 import { Main } from '../pages/main';
-//import { Product } from '../pages/product';
-
 
 test.beforeEach(async ({ page }) => {
-  const main = new Main(page);
-  await main.navigateTo();
+    const main = new Main(page);
+    await main.navigateTo();
 });
 
-
-test('Sign in from main page - positive', async ({ page }) => {
+test('Sign in from main page - positive - admin', { tag: '@regression' }, async ({ page }) => {
     const main = new Main(page);
 
     await main.fillUsername(process.env.ADMIN_USERNAME);
@@ -21,7 +18,18 @@ test('Sign in from main page - positive', async ({ page }) => {
     await expect(main.welcomeMsgLocator).toHaveText(`${main.partialWelcomeMsg}${process.env.ADMIN_USERNAME}`);
 });
 
-test('Sign in from main page - negative - username', async ({ page }) => {
+test('Sign in from main page - positive - user', { tag: '@regression' }, async ({ page }) => {
+    const main = new Main(page);
+
+    await main.fillUsername(process.env.USER_USERNAME);
+    await main.fillPassword(process.env.USER_PASSWORD);
+
+    await expect(main.loginButtonLocator).toContainText(main.loginButtonText);
+    main.clickSignInButton();
+    await expect(main.welcomeMsgLocator).toHaveText(`${main.partialWelcomeMsg}${process.env.USER_USERNAME}`);
+});
+
+test("Sign in from main page - negative - admin's username", { tag: '@regression' }, async ({ page }) => {
     const main = new Main(page);
 
     await main.fillUsername(main.invalidSignInValue);
@@ -33,9 +41,9 @@ test('Sign in from main page - negative - username', async ({ page }) => {
 
 });
 
-test('Sign in from main page - negative - password', async ({ page }) => {
+test("Sign in from main page - negative - admin's password", { tag: '@regression' }, async ({ page }) => {
     const main = new Main(page);
-    
+
     main.fillUsername(process.env.ADMIN_USERNAME);
     main.fillPassword(main.invalidSignInValue)
 
@@ -45,5 +53,27 @@ test('Sign in from main page - negative - password', async ({ page }) => {
 
 });
 
-//planuję dodać logowanie z poziomu każdej strony i wtedy zakomentuję beforeEach i wrzucę navigate to do testów main page
+test("Sign in from main page - negative - user's username", { tag: '@regression' }, async ({ page }) => {
+    const main = new Main(page);
+
+    await main.fillUsername(main.invalidSignInValue);
+    await main.fillPassword(process.env.USER_PASSWORD)
+
+    await expect(main.loginButtonLocator).toContainText(main.loginButtonText);
+    main.clickSignInButton();
+    await expect(main.loginErrorLocator).toHaveText(main.loginErrorText);
+
+});
+
+test("Sign in from main page - negative - user's password", { tag: '@regression' }, async ({ page }) => {
+    const main = new Main(page);
+
+    main.fillUsername(process.env.USER_USERNAME);
+    main.fillPassword(main.invalidSignInValue)
+
+    await expect(main.loginButtonLocator).toContainText(main.loginButtonText);
+    main.clickSignInButton();
+    await expect(main.loginErrorLocator).toHaveText(main.loginErrorText);
+
+});
 
