7f12d8305dafe47a067cf9011be589770ca8b742
adrmen_acxiom <adrianna.mendalka@acxiom.com>
2026-03-17 11:40:35 +0100

    update test for api post method

--- tests/api.spec.js
+++ tests/api.spec.js
@@ -4,12 +4,12 @@ import { Api } from '../pages/api';
 
 
 test.beforeEach(async ({ page }) => {
-  const api = new Api(page);
-  await api.navigateTo();
+    const api = new Api(page);
+    await api.navigateTo();
 });
 
 test('Test API - GET method - Item with id=3', async ({ request }) => {
-    const api = new Api(); 
+    const api = new Api();
     const responseGet3 = await request.get('/api/index.php?endpoint=products&id=3');
     expect(responseGet3.status()).toBe(200);
     expect(await responseGet3.text()).toContain(api.responseGetProductId3);
@@ -19,16 +19,27 @@ test('Test API - POST method', async ({ request }) => {
     const api = new Api();
     const responsePost = await request.post('/api/index.php?endpoint=products', {
         data: {
-            "name": "Test product",
-            "price": 119.99,
-            "currency": "PLN"
+            name: 'Test product',
+            price: 119.99,
+            currency: 'PLN'
         }
     });
 
     expect(responsePost.status()).toBe(201);
 
-    console.log(await responsePost.text())
-    //WORK IN PROGRESS - ADD ID at the end from the repsonse
-    expect(await responsePost.text()).toContain(api.apiPostResponse)
+
+    const bodyToJson = await responsePost.json();
+    const productId = bodyToJson.product.id;
+    console.log(`Product ID: ${productId}`);
+
+    expect(bodyToJson).toMatchObject({
+        message: 'created (mock)',
+        product: {
+            name: 'Test product',
+            price: 119.99,
+            currency: 'PLN',
+            id: productId,
+        },
+    });
 
 });
